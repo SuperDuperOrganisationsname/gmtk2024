d78b2abcfb0ea414e71fde0f4d81b3919b5d7e19
lukasgeis <lukasgeis@gmx.de>
2024-08-18 21:48:16 +0200

    key pickups

--- scripts/door.gd
+++ scripts/door.gd
@@ -3,21 +3,26 @@ extends Node2D
 signal complete_level
 
 @export var teleport: Vector2i = Vector2i(0, 0)
-@export var num_keys: int = 0
+@export var keys: Array[Node2D] = []
 
 var player_on_door: bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var num_keys = process_keys()
 	self.frame = num_keys
 	if num_keys == 0 and player_on_door and Input.is_action_just_pressed("enter_door"):
 		complete_level.emit()
 
+func process_keys() -> int:
+	var num_keys: int = 0
+	for key in keys:
+		if key.visible == true:
+			num_keys += 1
+		else:
+			key.process_mode = Node.PROCESS_MODE_DISABLED
+	
+	return num_keys
 
 func _on_door_entered(_body: Node2D) -> void:
 	player_on_door = true
