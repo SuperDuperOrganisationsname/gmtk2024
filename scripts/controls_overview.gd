16a48d877649b77c98eef141c6f9b624a9b0012f
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-08-19 15:21:05 +0200

    added controls overlay

--- scripts/controls_overview.gd
+++ scripts/controls_overview.gd
@@ -4,6 +4,11 @@ var controls_overlay_active = false
 
 func _ready() -> void:
     visible = controls_overlay_active
+
+func _process(delta: float) -> void:
+    if Input.is_action_just_released("close_controls") or Input.is_action_just_pressed("open_pause_menu"):
+        controls_overlay_active = false
+        visible = controls_overlay_active
     
 func toggle_controls_menu() -> void:
     controls_overlay_active = !controls_overlay_active
