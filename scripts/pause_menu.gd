6e7721fac66805a49bef0f59f1bd5839b8957d53
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-08-19 14:14:32 +0200

    changed button colors

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -9,5 +9,14 @@ func _on_player_toggle_pause_menu() -> void:
     pause_menu_active = !pause_menu_active
     visible = pause_menu_active
 
+func _on_continue_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_reset_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_controls_button_pressed() -> void:
+    pass # Replace with function body.
+
 func _on_quit_button_pressed() -> void:
     get_tree().quit()
