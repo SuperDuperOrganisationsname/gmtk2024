c3c108ba5594accd8c1b403a1763641e5aad9d63
lukasgeis <lukasgeis@gmx.de>
2024-08-19 14:55:36 +0200

    level placeholders 8-14

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,10 +3,10 @@ extends Control
 signal change_scene(name: String)
 
 func _on_start_button_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/game.tscn") 
+	get_tree().change_scene_to_file("res://scenes/game.tscn") 
 
 func _on_credits_button_pressed() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 func _on_quit_button_pressed() -> void:
-    get_tree().quit()
+	get_tree().quit()
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -5,30 +5,30 @@ signal pause_menu_reset_level
 var pause_menu_active = false
 
 func toggle_pause_menu() -> void:
-    pause_menu_active = !pause_menu_active
-    get_tree().paused = pause_menu_active
-    visible = pause_menu_active  
+	pause_menu_active = !pause_menu_active
+	get_tree().paused = pause_menu_active
+	visible = pause_menu_active  
 
 func _ready() -> void:
-    visible = pause_menu_active
+	visible = pause_menu_active
 
 func _process(delta: float) -> void:
-    if Input.is_action_just_pressed("open_pause_menu"):
-        toggle_pause_menu()
+	if Input.is_action_just_pressed("open_pause_menu"):
+		toggle_pause_menu()
 
 func _on_player_toggle_pause_menu() -> void:
-    toggle_pause_menu()
+	toggle_pause_menu()
 
 func _on_continue_button_pressed() -> void:
-    toggle_pause_menu()
+	toggle_pause_menu()
 
 func _on_reset_button_pressed() -> void:
-    toggle_pause_menu()
-    pause_menu_reset_level.emit()
+	toggle_pause_menu()
+	pause_menu_reset_level.emit()
 
 func _on_controls_button_pressed() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 func _on_quit_button_pressed() -> void:
-    toggle_pause_menu()
-    get_tree().change_scene_to_file("res://scenes/main_menu.tscn") 
+	toggle_pause_menu()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn") 
