0c5e2441829a1b380a93f52024159fb85865d036
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-08-19 23:59:21 +0200

    main menu start plays intro cutscene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 signal change_scene(name: String)
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn") 
+	get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn") 
 
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits_menu.tscn")
