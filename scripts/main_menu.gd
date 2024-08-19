ea10a2890474ad4ab32e795881e315bc8f0daca8
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-08-19 11:44:24 +0200

    added main menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+signal change_scene(name: String)
+
+func _on_start_button_pressed() -> void:
+    get_tree().change_scene_to_file("res://scenes/game.tscn") 
+
+func _on_credits_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+    get_tree().quit()
