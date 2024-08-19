53a13bc1f00cf5e7b0c83cda90b5ed3eebd8bb1f
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-08-19 15:47:52 +0200

    made button on credits page functional

--- scripts/credits_menu.gd
+++ scripts/credits_menu.gd
@@ -2,4 +2,7 @@ extends Control
 
 func _process(delta: float) -> void:
     if Input.is_action_just_pressed("open_pause_menu") or Input.is_action_just_pressed("close_controls"):
-        get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+        get_tree().change_scene_to_file("res://scenes/main_menu.tscn") 
+
+func _on_button_pressed() -> void:
+    get_tree().change_scene_to_file("res://scenes/main_menu.tscn") 
