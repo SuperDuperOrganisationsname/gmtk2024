97ec764b30b8405ff2f457b033be069ad56af4b8
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-08-19 15:03:56 +0200

    made controls overview closable

--- scripts/controls_overview.gd
+++ scripts/controls_overview.gd
@@ -1,16 +1,17 @@
 extends Control
 
-var pause_controls_active = false
+var controls_overlay_active = false
 
 func _ready() -> void:
-    visible = pause_controls_active
+    visible = controls_overlay_active
     
 func toggle_controls_menu() -> void:
-    pause_controls_active = !pause_controls_active
-    visible = pause_controls_active 
+    controls_overlay_active = !controls_overlay_active
+    visible = controls_overlay_active 
 
 func _on_close_button_pressed() -> void:
     toggle_controls_menu()
 
 func _on_pause_menu_pause_menu_open_controls() -> void:
-    pass
+    controls_overlay_active = true
+    visible = controls_overlay_active
