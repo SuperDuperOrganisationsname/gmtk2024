7ed34e49f2eda9b8d7ec495990b9a28d8cb4b52d
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-08-20 14:06:52 +0200

    main menu controllable with buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,6 +4,14 @@ signal change_scene(name: String)
 
 @onready var click_sound: AudioStreamPlayer2D = $ClickSound
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("open_pause_menu"):
+		self._on_quit_button_pressed()
+	if Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("ui_accept"):
+		self._on_start_button_pressed()
+	if Input.is_action_just_pressed("attack"):
+		self._on_credits_button_pressed()	
+
 func _on_start_button_pressed() -> void:
 	click_sound.play()	
 	get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn") 
