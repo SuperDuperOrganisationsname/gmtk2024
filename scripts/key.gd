3f6cc39a402c0384593e2a0822f0789edb0311fa
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-08-20 10:22:17 +0200

    tried to add sfx to key

--- scripts/key.gd
+++ scripts/key.gd
@@ -1,4 +1,9 @@
 extends Node2D
 
+@onready var animation_player = $AnimationPlayer
+
+func _ready() -> void:
+	animation_player.play("RESET")
+
 func _on_player_entered(body: Node2D) -> void:
-	self.visible = false
+	animation_player.play("pickup_animation")
