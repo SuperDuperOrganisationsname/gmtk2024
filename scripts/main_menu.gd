987c72c02bc8127cba687ba234d14cbfed93d82a
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-08-20 10:42:36 +0200

    broken click sound in main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,10 +2,14 @@ extends Control
 
 signal change_scene(name: String)
 
+@onready var click_sound: AudioStreamPlayer2D = $ClickSound
+
 func _on_start_button_pressed() -> void:
+	click_sound.play()	
 	get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn") 
 
 func _on_credits_button_pressed() -> void:
+	click_sound.play()
 	get_tree().change_scene_to_file("res://scenes/credits_menu.tscn")
 
 func _on_quit_button_pressed() -> void:
